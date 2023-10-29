b2f4a27a590d2ffcc787831144d65e21927c4f6e
shredinjohn <94382938+shredinjohn@users.noreply.github.com>
2023-10-29 23:36:26 +0530

    Update

--- my-app/test.py
+++
@@ -1,5 +0,0 @@
-from bardapi import Bard
-
-token = 'aQjr2eQeEWQE-72zWztqA1l2R_mDGBhLWEfZA-QDA18infSwkXutpNnR6LBWAJYySJbCLA.'
-bard = Bard(token=token)
-print(bard.get_answer("hey what")['content'])
