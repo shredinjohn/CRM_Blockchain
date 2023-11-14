eb4573fc18c16ad7a79e94db56f3c52a92ba53f9
vigneshmagalingam <vigneshmagalingamwork@gmail.com>
2023-11-14 10:17:34 +0530

    Added Sidebar and Hamburger menu (Frontend)

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,23 @@
 import React from 'react'
+import { Route , Routes } from 'react-router-dom';
 
-function App() {
+import { Home, CreateCampaign, CampaignDetails, Profile } from './pages';
+import { Navbar, Sidebar } from './components';
+
+
+const App= () => {
   return (
-    <div>App ok</div>
+    <div className="relative sm:-8 p-4 bg-[#13131a] min-h-screen flex flex-row" >
+        <div className="sm:flex hidden mr-10 relative"> <Sidebar /> </div>
+        <div className="flex-1 max-sm:w-full max-w-[1280px] mx-auto sm:pr-5"> <Navbar /> 
+        <Routes>
+            <Route path="/" element={<Home />}></Route>
+            <Route path="/create-campaign" element= {<CreateCampaign />}></Route>
+            <Route path="/profile" element= {<Profile />}></Route>
+            <Route path="/campaign-details/:id" element= {<CampaignDetails />}></Route>
+        </Routes>
+        </div>
+    </div>
   )
 }
 
--- client/src/index.jsx
+++ client/src/index.jsx
@@ -3,13 +3,14 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter as Router} from "react-router-dom";
 import App from "./App";
 import { ChainId, ThirdwebProvider } from '@thirdweb-dev/react';
+import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
     <ThirdwebProvider desiredchainId={ChainId.Sepolia}>
         <Router>
-        <div>Check Check</div>
-        </Router>
+        <App />
+        </Router> 
     </ThirdwebProvider>
     );
